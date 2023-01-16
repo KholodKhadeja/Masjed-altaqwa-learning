4d26a3dafae35b98e8b655522de435b82253b02c
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-01-16 21:07:55 +0200

    code work on

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from '../src/images/final-logo.png';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Hello World!!!!!</h1>
+    </div>
+  );
+}
+
+export default App;
